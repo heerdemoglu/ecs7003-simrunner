e1b5c277011a9f2e87166c196cce1a156160d11f
Hasan Emre Erdemoğlu <heerdemoglu@users.noreply.github.com>
2020-10-27 19:37:09 +0000

    pcg demo

--- ecs7003-simrunner-proto/Assets/Scripts/CameraController.cs
+++ ecs7003-simrunner-proto/Assets/Scripts/CameraController.cs
@@ -7,6 +7,9 @@ public class CameraController : MonoBehaviour
 	public GameObject player;
 	private Vector3 offset;
 
+	public float horizontalSpeed = 2.0F;
+	public float verticalSpeed = 2.0F;
+
 	// Start is called before the first frame update
 	void Start()
 	{
@@ -16,6 +19,14 @@ public class CameraController : MonoBehaviour
 	void LateUpdate()
 	{
 		transform.position = player.transform.position + offset;
+	}
+
+	void Update()
+    {
+		float h = horizontalSpeed * Input.GetAxis("Mouse X");
+		float v = verticalSpeed * Input.GetAxis("Mouse Y");
+
+		transform.Rotate(0, 0, 0); // not being used atm (v;h;0)
 	}
 
 
---
+++ ecs7003-simrunner-proto/Assets/Scripts/TileManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+
+    public GameObject tilePrefab;
+    public float xSpawn = 0; // Spawning tile
+    public float tileLength;
+    public int noOfTiles;
+
+    public Transform playerTransform;
+    public Vector3 pos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < noOfTiles; i++)
+        {
+            spawnTile();
+        }
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerTransform.position.x > xSpawn - noOfTiles*tileLength) // debug this
+        {
+            // Add mechanic to remove old tiles as well?
+            // Reuse old tiles; like teleport gates?
+            spawnTile();
+
+        }
+    }
+
+    public void spawnTile()
+    {
+        Instantiate(tilePrefab, transform.right * xSpawn, transform.rotation); 
+        xSpawn += tileLength;
+    }
+
+}
