eb58c4e05b582724bf6bb7fd11b277ab1595e521
Hasan Emre Erdemoğlu <heerdemoglu@users.noreply.github.com>
2020-10-28 15:58:26 +0000

    Camera Movement
    Implemented camera panning. X and Y directions. Y direction has to be clamped. (TODO)

--- ecs7003-simrunner-proto/Assets/Scripts/CameraController.cs
+++ ecs7003-simrunner-proto/Assets/Scripts/CameraController.cs
@@ -5,18 +5,26 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
 	public GameObject player;
-	private Vector3 offset;
+	private Vector3 camOffset;
+
+	public float speed = 2.0f;
 
 	// Start is called before the first frame update
 	void Start()
 	{
-		offset = transform.localPosition;
-	}
-	
-	void LateUpdate()
-	{
-		transform.position = player.transform.position + offset;
+		camOffset = new Vector3(player.transform.position.x-16f, player.transform.position.y+7f, player.transform.position.z+4);
+
+		// Lock the cursor to the middle of the screen.
+		Cursor.lockState = CursorLockMode.Locked;
 	}
 
+	void Update()
+	{
+		// Use Quarternions to rotate the camera:
+		camOffset = Quaternion.AngleAxis(-Input.GetAxis("Mouse Y") * speed, Vector3.right) * camOffset;
+		camOffset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * speed, Vector3.up) * camOffset;
+		transform.position = player.transform.position + camOffset;
+		transform.LookAt(player.transform.position);
+	}
 
 }
