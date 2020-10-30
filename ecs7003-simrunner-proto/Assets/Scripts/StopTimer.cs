d4dd4a21b60b9c35d6fd5a7a1f7eb9dd8b0a84db
Xsara Baptiste-Wilson <x.baptiste-wilson@se18.qmul.ac.uk>
2020-10-30 16:15:05 +0000

    stops timer
    stops timer  when entering a collider

---
+++ ecs7003-simrunner-proto/Assets/Scripts/StopTimer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class StopTimer : MonoBehaviour
+{
+    private TimerController timeController;
+    private void Start()
+    {
+        GameObject gameC = GameObject.FindGameObjectWithTag("TimerController");
+        timeController = gameC.GetComponent<TimerController>();
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        timeController.StopTime();
+        
+    }
+}
--- ecs7003-simrunner-proto/Assets/Scripts/TimerController.cs
+++ ecs7003-simrunner-proto/Assets/Scripts/TimerController.cs
@@ -1,30 +1,36 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 public class TimerController : MonoBehaviour
 {
     public Text timerText;
-    public float time;
-    public float startTime;
+    private float startTime;
+    private bool stopTimer = false;
     // Start is called before the first frame update
     void Start()
     {
         startTime = Time.time;
-        Debug.Log(System.DateTime.Now.ToString());
     }
 
     // Update is called once per frame
     void Update()
-    {
+    {   if (stopTimer)
+            return;
         float timer = Time.time - startTime;//in seconds
         
 
         int minutes = (int)timer / 60; //amount of minutes
         int seconds = (int)timer % 60;//remainder seconds from minutes
-        //int fraction = (int)(timer * 100) % 100;
-        timerText.text = string.Format("{0:00} : {1:00} ", minutes, seconds);
+        int fraction = (int)(timer * 100) % 100;
+        timerText.text = string.Format("{0:00} : {1:00} : {2:00}", minutes, seconds, fraction);
         
+    }
+    public void StopTime()
+    {
+        stopTimer = true;
+        timerText.color = Color.red;
     }
 }
 
