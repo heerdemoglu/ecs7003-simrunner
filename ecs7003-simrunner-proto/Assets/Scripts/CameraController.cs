bc1b43cf57dd6950e605e82c3897afce7b4d6370
Xsara Baptiste-Wilson <x.baptiste-wilson@se18.qmul.ac.uk>
2020-10-30 02:27:31 +0000

    basic timer
    added basic timer that start at the beginning of the game

--- ecs7003-simrunner-proto/Assets/Scripts/CameraController.cs
+++ ecs7003-simrunner-proto/Assets/Scripts/CameraController.cs
@@ -1,68 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
-using System.Net;
 using System.Security.Cryptography;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-	public GameObject player;
-	private Vector3 offset;
-    
-	private float sensitivity = 0.01f;
+    public GameObject player;//create a public game object
+    private Vector3 offset;
 
-	private float yaw = 0;
-	private float pitch = 0;
+    void Start() //called before first frame update
+    {
+        offset = transform.position;//inistialise the variable offset with the initial position of the camera
+        //we can directly acces transform through the variable transform
+    }
 
-	float rotX = 0.0f;
-	float rotY = 0.0f;
-
-
-	void Start()
-	{
-
-		// Lock the cursor to the middle of the screen.
-		Cursor.lockState = CursorLockMode.Locked;
-
-		// Set camera to follow the player (distance btwn camera and player)
-		// Set camera to follow the player (distance btwn camera and player)
-		offset = transform.position - player.transform.position; // (back(+) ,down(-), right(-))
-		rotX = transform.eulerAngles.x;
-		rotY = transform.eulerAngles.y;
-	}
-
-
-	void Update()
-	{
-
-		float xAxis = Input.GetAxis("Mouse X"); //0,11, best clamp angles
-		float yAxis = Input.GetAxis("Mouse Y"); // 262, 277, best clamp angles
-
-		yaw += sensitivity * xAxis;
-		pitch += sensitivity * yAxis;
-
-		rotY += yaw;
-		rotX -= pitch;
-
-		rotX = moduloAngleClamp(rotX, 20, 30);
-		rotY = moduloAngleClamp(rotY, 250, 290);
-
-		transform.rotation = Quaternion.Euler(rotX, rotY, 0);
-
-		// Use Quarternions to rotate the camera:
-		transform.position = player.transform.position + offset;
-		//transform.LookAt(player.transform.position);
-
-	}
-
-
-	public static float moduloAngleClamp(float angle, float min, float max)
-	{
-		if (angle < -360f)
-			angle += 360f;
-		if (angle > 360f)
-			angle -= 360f;
-		return Mathf.Clamp(angle, min, max);
-	}
+    //Any update thate happen after movement, used for procedural animations and gathering last known states
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
 }
---
+++ ecs7003-simrunner-proto/Assets/Scripts/TimerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class TimerController : MonoBehaviour
+{
+    public Text timerText;
+    public float time;
+    public float startTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Time.time;
+        Debug.Log(System.DateTime.Now.ToString());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float timer = Time.time - startTime;//in seconds
+        
+
+        int minutes = (int)timer / 60; //amount of minutes
+        int seconds = (int)timer % 60;//remainder seconds from minutes
+        //int fraction = (int)(timer * 100) % 100;
+        timerText.text = string.Format("{0:00} : {1:00} ", minutes, seconds);
+        
+    }
+}
+
