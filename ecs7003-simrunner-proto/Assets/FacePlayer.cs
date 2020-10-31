d673cf197ee6f09301d7734e97b0fdef4352e34a
Xsara Baptiste-Wilson <x.baptiste-wilson@se18.qmul.ac.uk>
2020-10-31 21:06:46 +0000

    bot Moving between 2 points  and facing player update
    bot moving back and forth between two points and faces player when they are within its boundary

---
+++ ecs7003-simrunner-proto/Assets/FacePlayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class FacePlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private GameObject bot;
+    void Start()
+    {
+        BotMovement botM = GetComponent<BotMovement>();
+        bot = GameObject.FindGameObjectWithTag("Bot");
+    }
+
+    //IEnumerator moveBot()
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerStay(Collider other)
+    {
+
+        if (other.gameObject.tag == "Player")
+        {/*
+            botM.InRange();*/
+            bot.transform.LookAt(other.gameObject.transform.position);
+            UnityEngine.Debug.Log("in collider");
+        }
+    }
+}
