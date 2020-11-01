849284b40e83977c368ecbd5f33f7a138ea278a7
Xsara Baptiste-Wilson <x.baptiste-wilson@se18.qmul.ac.uk>
2020-11-01 18:52:19 +0000

    stop time when player dead

--- ecs7003-simrunner-proto/Assets/Scripts/StopTimer.cs
+++ ecs7003-simrunner-proto/Assets/Scripts/StopTimer.cs
@@ -2,14 +2,24 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StopTimer : MonoBehaviour
 {
+    public Text gameOver;
     private GameController gameController;
     private void Start()
     {
         GameObject gameC = GameObject.FindGameObjectWithTag("GameController");
         gameController = gameC.GetComponent<GameController>();
+    }
+     void Update()
+    {
+        if (gameOver.text == "YOU DIED")
+        {
+            gameController.StopTime();
+            return;
+        }
     }
     void OnTriggerEnter(Collider other)
     {
