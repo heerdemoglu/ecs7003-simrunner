11ec3e7bcda5126615f81cb8971bfc66cd755a05
Xsara Baptiste-Wilson <x.baptiste-wilson@se18.qmul.ac.uk>
2020-11-01 13:09:39 +0000

    bullet update

--- ecs7003-simrunner-proto/Assets/Scripts/FacePlayer.cs
+++ ecs7003-simrunner-proto/Assets/Scripts/FacePlayer.cs
@@ -10,62 +10,51 @@ public class FacePlayer : MonoBehaviour
 
     private GameObject bot;
     public GameObject bullet;
-    // public Vector3 spawnValues;
+
     public Transform spawnValues;
 
-    //public float startWait;
     public float spawnWait;
-    //public float waveWait;
     private bool inRange = false;
+
+    void Start()
+        {
+
+            BotMovement botM = GetComponent<BotMovement>();
+            bot = GameObject.FindGameObjectWithTag("Bot");
+            StartCoroutine(SpawnBullet());
+        }
+
     IEnumerator SpawnBullet()
     {
         
-        //yield return new WaitForSeconds(startWait);
-        while ( true)//
+        while ( true)
         {
-            //if (inRange == true)
-            //{
+            //while player in range spawn bullet from the bulletbarrel(spawnValues)
             yield return new WaitUntil(() => inRange==true);
 
             Vector3 spawnPosition = spawnValues.position;
-            //UnityEngine.Debug.Log("spawn bullet");
             Quaternion spawnRotation = Quaternion.identity;
             Instantiate(bullet, spawnValues.position, spawnRotation);
             yield return new WaitForSeconds(spawnWait);
-           // }
             
         }
-        //yield return new WaitForSeconds(spawnWait);
+        
     }
 
-    void Start()
-    {
+    
 
-        BotMovement botM = GetComponent<BotMovement>();
-        bot = GameObject.FindGameObjectWithTag("Bot");
-        StartCoroutine(SpawnBullet());
-    }
-
-    //IEnumerator moveBot()
-    // Update is called once per frame
     void Update()
     {
-        //if(inRange == true)
-        //    StartCoroutine(SpawnBullet());
-        //else
-        //    StopCoroutine(SpawnBullet());
+       
     }
+
     void OnTriggerStay(Collider other)
     {
         
-
         if (other.gameObject.tag == "Player")
-        {/*
-            botM.InRange();*/
-            //StartCoroutine(SpawnBullet());
+        {
             inRange = true;
             bot.transform.LookAt(other.gameObject.transform.position);
-            //UnityEngine.Debug.Log("in collider");
         }
     }
 
@@ -73,9 +62,8 @@ public class FacePlayer : MonoBehaviour
     {
         if (other.gameObject.tag == "Player")
         {
-
             inRange = false;
         }
     }
-    //
+    
 }
