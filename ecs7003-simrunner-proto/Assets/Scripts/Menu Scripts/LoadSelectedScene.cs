5f3673e88d2c4a962720e144d50b8f0cddbe0943
Xsara Baptiste-Wilson <x.baptiste-wilson@se18.qmul.ac.uk>
2020-11-27 18:56:27 +0000

    play button work at runtime

---
+++ ecs7003-simrunner-proto/Assets/Scripts/Menu Scripts/LoadSelectedScene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSelectedScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    public void LoadScene(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
