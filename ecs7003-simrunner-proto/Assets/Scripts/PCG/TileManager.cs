0a6a3e535205df9857dd6202619e52ff2bf85c53
Hasan Emre Erdemoğlu <heerdemoglu@users.noreply.github.com>
2020-11-27 17:17:58 +0000

    Game Bound Generation
    Generates infinite bounds as game progresses.
    
    TODO: Addition of randomly placed tiles within game space.

---
+++ ecs7003-simrunner-proto/Assets/Scripts/PCG/TileManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+
+    public GameObject BoundaryTile;
+    public float fwdSpawn;
+    public float tileLen;
+    public Transform playerTransform;
+    public int noOfAliveBounds;
+    public float spawnOffset;
+
+    private List<GameObject> aliveTiles = new List<GameObject>();
+
+    /**
+     * Starts the game with a single game boundary. The rest is built as player moves.
+     */
+    void Start()
+    {
+        // Start with a game boundary:
+        for (int i = 0; i < noOfAliveBounds; i++)
+        {
+            SpawnGameBoundary();
+        }
+    }
+
+    /**
+     * As player moves spawn new game boundary and kill the old ones.
+     */
+    void Update()
+    {
+        Debug.Log(playerTransform.position.z);
+        if (playerTransform.position.z - spawnOffset > (fwdSpawn - noOfAliveBounds * tileLen))
+        {
+            SpawnGameBoundary();
+            DeleteBoundary();
+        }
+    }
+
+    /**
+     * Spawns a tile and adds its to the set of alive tiles.
+     */
+    public void SpawnGameBoundary()
+    {
+        GameObject boundary = Instantiate(BoundaryTile, transform.forward * fwdSpawn, transform.rotation);
+        aliveTiles.Add(boundary);
+        fwdSpawn += tileLen;
+    }
+
+    /**
+     * Destroys the boundary from the starting position to keep memory in check.
+     */
+    private void DeleteBoundary()
+    {
+        Destroy(aliveTiles[0]);
+        aliveTiles.RemoveAt(0);
+    }
+}
