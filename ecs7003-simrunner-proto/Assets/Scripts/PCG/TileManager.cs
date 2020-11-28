81532f084d58afdf2e939cd5367e90f4c701be5f
Hasan Emre Erdemoğlu <heerdemoglu@users.noreply.github.com>
2020-11-28 12:36:49 +0000

    Random Wall Spawning Initial Commit
    Spawns random walls. Needs fine tuning along with movement mechanics.

--- ecs7003-simrunner-proto/Assets/Scripts/PCG/TileManager.cs
+++ ecs7003-simrunner-proto/Assets/Scripts/PCG/TileManager.cs
@@ -6,12 +6,19 @@ public class TileManager : MonoBehaviour
 {
 
     public GameObject BoundaryTile;
+    public GameObject[] tiles; // Array used for different type of tiles (if available)
+
     public float fwdSpawn;
     public float tileLen;
     public Transform playerTransform;
+    
     public int noOfAliveBounds;
-    public float spawnOffset;
+    public int noOfTilesPerBound;
 
+    public float spawnOffset;
+    public Vector3 spawnTileValues;
+
+    private List<GameObject> aliveBounds = new List<GameObject>();
     private List<GameObject> aliveTiles = new List<GameObject>();
 
     /**
@@ -23,6 +30,7 @@ public class TileManager : MonoBehaviour
         for (int i = 0; i < noOfAliveBounds; i++)
         {
             SpawnGameBoundary();
+            SpawnTiles();
         }
     }
 
@@ -31,11 +39,13 @@ public class TileManager : MonoBehaviour
      */
     void Update()
     {
-        Debug.Log(playerTransform.position.z);
         if (playerTransform.position.z - spawnOffset > (fwdSpawn - noOfAliveBounds * tileLen))
         {
             SpawnGameBoundary();
-            DeleteBoundary();
+            SpawnTiles();
+
+            DestroyBoundary();
+            DestroyTiles();
         }
     }
 
@@ -45,16 +55,50 @@ public class TileManager : MonoBehaviour
     public void SpawnGameBoundary()
     {
         GameObject boundary = Instantiate(BoundaryTile, transform.forward * fwdSpawn, transform.rotation);
-        aliveTiles.Add(boundary);
+        aliveBounds.Add(boundary);
         fwdSpawn += tileLen;
     }
+
+/**
+ * Randomly spawns tiles in the bounded area.
+ * 
+ */
+    public void SpawnTiles()
+    {
+        for (int i = 0; i < noOfTilesPerBound; i++)
+        {
+
+            Vector3 tilePos = new Vector3(Random.Range(-spawnTileValues.x, spawnTileValues.x),
+                Random.Range(0, spawnTileValues.y),
+                Random.Range(-spawnTileValues.z, spawnTileValues.z));
+
+            // Rotation can be added as well:
+            GameObject tile = Instantiate(tiles[0], tilePos + transform.forward * (fwdSpawn-tileLen),
+                Quaternion.Euler(new Vector3(0, 90, 0)));
+            aliveTiles.Add(tile);
+        }
+    }
+
 
     /**
      * Destroys the boundary from the starting position to keep memory in check.
      */
-    private void DeleteBoundary()
+    private void DestroyBoundary()
     {
-        Destroy(aliveTiles[0]);
-        aliveTiles.RemoveAt(0);
+        Destroy(aliveBounds[0]);
+        aliveBounds.RemoveAt(0);
+    }
+
+    /**
+     * Removes first N tiles we left behind.
+     * 
+     */
+    public void DestroyTiles()
+    {
+        for (int i = 0; i < noOfTilesPerBound; i++)
+        {
+            Destroy(aliveTiles[0]);
+            aliveTiles.RemoveAt(0);
+        }
     }
 }
