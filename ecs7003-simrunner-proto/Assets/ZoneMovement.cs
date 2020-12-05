9fc6ce6d09ac7c5066a4885feeb81c948befbe13
Xsara Baptiste-Wilson <x.baptiste-wilson@se18.qmul.ac.uk>
2020-12-05 17:18:53 +0000

    fixing merge

--- ecs7003-simrunner-proto/Assets/ZoneMovement.cs
+++ ecs7003-simrunner-proto/Assets/ZoneMovement.cs
@@ -2,22 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Zone : MonoBehaviour
+public class ZoneMovement : MonoBehaviour
 {
 
     private GameObject player;
     public float speed;
-
     private bool chasing = false;
     private float acceleration;
-
     public float initialDistance = 15f;
     // Update is called once per frame
     void Start()
     {
-        Vector3 initialVector = new Vector3(0,0, initialDistance);
+
         player = GameObject.FindGameObjectWithTag("Player");
-        transform.position = player.transform.position - initialVector;
+        //transform.position.z = player.transform.position.z - initialDistance;
 
     }
     void Update()
