3058982813283886b31916ddbd15541748b86ac1
Xsara Baptiste-Wilson <x.baptiste-wilson@se18.qmul.ac.uk>
2020-12-07 05:25:49 +0000

    camerashake on click

--- ecs7003-simrunner-proto/Assets/Scripts/Game Control Scripts/Chased.cs
+++ ecs7003-simrunner-proto/Assets/Scripts/Game Control Scripts/Chased.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using EZCameraShake;
 public class Chased : MonoBehaviour
 {
     public GameObject gameLight;
@@ -49,7 +49,11 @@ public class Chased : MonoBehaviour
     }
     void Update()
     {
-
+        if (Input.GetMouseButtonDown(0))
+        {
+            CameraShaker.Instance.ShakeOnce(4f, 4f, 1f, 1f);
+        }
+        
         float zoneZ = zone.transform.position.z;
         distanceToZone = Mathf.Abs(transform.position.z - zoneZ);
         
